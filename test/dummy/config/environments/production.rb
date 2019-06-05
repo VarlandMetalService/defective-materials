cd4f0395bb415ac71744cf1fabb6b60a52096a30
Richard Legacy <richard.legacy@varland.com>
2019-06-05 18:12:07 -0400

    assets?

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -28,7 +28,11 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.digest = true 
   config.serve_static_assets = true
+
+  config.assets.precompile += %w(dmr/application.js dmr/application.css)
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
