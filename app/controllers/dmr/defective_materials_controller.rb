8c72c72076d25b8ff9955578835586dbf11f5990
Richard Legacy <richard.legacy@varland.com>
2019-06-05 17:45:00 -0400

    maybe?

--- app/controllers/dmr/defective_materials_controller.rb
+++ app/controllers/dmr/defective_materials_controller.rb
@@ -8,7 +8,7 @@ module Dmr
 
     def get_so_info
       @current_user = current_user
-      uri = URI("http://localhost:3001/defective_materials/query_so?so_num=#{params[:so_num]}")
+      uri = URI("http://192.168.82.230/defective_materials/query_so?so_num=#{params[:so_num]}")
       response = Net::HTTP.get(uri)
       @shop_order = Presenter.hash_to_objects(response).first
       respond_to do |format|
@@ -17,7 +17,7 @@ module Dmr
     end
 
     def show
-      uri = URI("http://localhost:3001/defective_materials/#{params[:id]}")
+      uri = URI("http://192.168.82.230/defective_materials/#{params[:id]}")
       @response = Net::HTTP.get(uri)
       @dmr = Presenter.hash_to_objects(@response)
       respond_to do |format|
