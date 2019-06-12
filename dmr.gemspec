5e54074cdfbafd98cb90f4592d117acbb0116fe8
rlegacy-varland <richard.legacy@varland.com>
2019-06-12 14:48:28 +0000

    precompile assets?

--- dmr.gemspec
+++ dmr.gemspec
@@ -6,7 +6,7 @@ require "dmr/version"
 # Describe your gem and declare its dependencies:
 Gem::Specification.new do |spec|
   spec.name        = "dmr"
-  spec.version     = 1.5
+  spec.version     = 1.51
   spec.authors     = ["Richard Legacy"]
   spec.email       = ["richard.legacy@varland.com"]
   spec.homepage    = ""
--- lib/dmr/engine.rb
+++ lib/dmr/engine.rb
@@ -3,11 +3,19 @@ module Dmr
     isolate_namespace Dmr
 
     initializer :assets do |config|
-      Rails.application.config.assets.paths << root.join("app", "assets", "images", "dmr")
+      Rails
     end
 
-    initializer "dmr.assets.precompile" do |app|
+    initializer "dmr.precompile" do |app|
+      app.config.assets.paths << root.join("app", "assets", "images", "dmr")
       app.config.assets.precompile += %w( dmr/* )
     end
+
+    initializer "my_engine.precompile" do |app|
+      app.config.assets.paths << Rails.root.join('/engines/dmr/app/assets/*')
+      app.config.assets.precompile << "dmr/*"
+    end
+
+
   end
 end
