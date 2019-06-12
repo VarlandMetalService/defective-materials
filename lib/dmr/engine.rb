eb961d5c8b237459687865bdb78e53ee4fe8ee78
rlegacy-varland <richard.legacy@varland.com>
2019-06-12 15:06:28 +0000

    assets...

--- lib/dmr/engine.rb
+++ lib/dmr/engine.rb
@@ -2,11 +2,12 @@ module Dmr
   class Engine < ::Rails::Engine
     isolate_namespace Dmr
 
-    Rails.application.config.assets.paths << "app/assets/*"
+    Rails.application.
     initializer "dmr.assets.precompile" do |app|
+      app.config.assets.paths << "app/assets/*"
       app.config.assets.precompile += %w( dmr/* dmr.scss dmr.js )
+      app.config.assets.precompile += ['*.js', '*.css', '**/*.js', '**/*.css', '*.jpg', '*.png', '*.ico', '*.gif', '*.woff2', '*.eot', '*.woff', '*.ttf', '*.svg'] 
     end
-    Rails.application.config.assets.precompile += ['*.js', '*.css', '**/*.js', '**/*.css', '*.jpg', '*.png', '*.ico', '*.gif', '*.woff2', '*.eot', '*.woff', '*.ttf', '*.svg'] 
 
   end
 end
